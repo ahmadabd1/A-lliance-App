6a51185ba057e85fee55567eaf9b055adca1da0b
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-16 14:05:39 +0200

    update filter.js file & add route category

--- server/model/filterData.js
+++ server/model/filterData.js
@@ -1,3 +1,28 @@
+const saveDb = require("../model/saveDataBase");
+const DB = new saveDb();
+
 class DataHandling {
-  FilterData() {}
+  async FilterData(promise, res) {
+    try {
+      const resp = await promise;
+      const _data = resp.data.results.map((key) => {
+        return {
+          difficulty: key.difficulty,
+          category: key.category,
+          type: key.type,
+          question: key.question,
+          correct_answer: key.correct_answer,
+          incorrect_answers: key.incorrect_answers,
+        };
+      });
+      DB.saveDataToDatabase(_data);
+      console.log(_data);
+      res.send(_data);
+    } catch (error) {
+      console.error("Error in Fetching Data: ", error);
+      res.status(404).send({ error: "Try again!" });
+    }
+  }
 }
+
+module.exports = DataHandling;
