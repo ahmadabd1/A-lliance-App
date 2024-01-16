4bbac63e442d8585cc89167ff78990a498e8a500
Naarani <n3ra96@gmail.com>
2024-01-16 10:27:58 +0200

    added constructer to board.js , organized apimanger.js, connected file scripts to index.html

--- dist/board.js
+++ dist/board.js
@@ -1,2 +1,21 @@
-console.log("welak allloooo")
-console.log("welak allllllloooooo")
+
+class board{
+    constructor(boardSize) {
+        this.players = [];
+        this.currentPlayerIndex = 0;
+       // this.apiManager = new ApiManager();
+       // this.questionHandler = new QuestionHandler(this.apiManager, this);
+        this.dice = new Dice();
+        this.dumbot = new Dumbot("DumBot");
+        this.player = new Player("Naarani");
+        this.boardSize = boardSize;
+        this.totalCells = boardSize * boardSize;
+        this.questionsInCells = {};
+        this.maxQuestions = 10;
+    }
+
+
+}
+
+
+const t = new board(10)
