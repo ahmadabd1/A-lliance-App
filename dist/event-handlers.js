b0f9ae49ba5ed13080636bb81477270771b4b176
RawabyRahal <rawaby.16.4@gmail.com>
2024-01-16 12:54:46 +0200

    added volume and  dice

--- dist/event-handlers.js
+++ dist/event-handlers.js
@@ -3,3 +3,15 @@
 $("#checkbox").on("change", function () {
     $("body").toggleClass("dark");
 });
+
+async function rollDice(number) {
+    const dice = await[...document.querySelectorAll(".die-list")];
+    return dice.forEach(die => {
+        toggleClasses(die);
+        die.dataset.roll = number;
+    });
+}
+function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+}
