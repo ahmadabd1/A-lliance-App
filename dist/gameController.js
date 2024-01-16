adf815a351160c0a24140f47bb8b4f54ef6e3138
Naarani <n3ra96@gmail.com>
2024-01-16 12:01:27 +0200

    added game logic with these functions: constructer(), start() , startGame(), startTurn() , startDumbotTurn() ,nextTurn()

---
+++ dist/gameController.js
@@ -0,0 +1,60 @@
+class GameController {
+    constructor(boardSize) {
+        this.boardSize = boardSize;
+        this.gameBoard = new Board(this.boardSize);
+    }
+
+    start() {
+        this.gameBoard.initializeBoard();
+        this.gameBoard.startGame();
+    }
+
+    startGame() {
+        this.startTurn();
+    }
+    
+    startTurn() {
+        console.log('startTurn called');
+        const currentPlayer = this.gameBoard.getCurrentPlayer()
+        this.gameBoard.displayPlayerTurn(currentPlayer)
+        currentPlayer.rollValue = 0
+        
+        if (currentPlayer instanceof Player && !(currentPlayer instanceof Dumbot)) {
+            rollDiceButton.off('click').on('click', () => {
+                currentPlayer.rollValue = this.gameBoard.dice.rollDice()
+                rollDice(currentPlayer.rollValue)
+                this.gameBoard.movePlayer(currentPlayer, currentPlayer.rollValue)
+                this.gameBoard.displayPlayerTurn(this.gameBoard.getCurrentPlayer())
+                setTimeout(() => {
+                    this.nextTurn();
+                }, 1000);
+            });
+        } else {
+            this.startDumbotTurn()
+        }
+    }
+
+    startDumbotTurn() {
+        console.log('startDumbotTurn called');
+        const currentPlayer = this.gameBoard.getCurrentPlayer();
+        currentPlayer.rollValue = 0;
+        currentPlayer.rollValue = this.gameBoard.dice.rollDice()
+        this.gameBoard.movePlayer(currentPlayer, currentPlayer.rollValue)
+        this.gameBoard.displayPlayerTurn(currentPlayer)
+
+        setTimeout(() => {
+            this.nextTurn();
+            // rollDiceButton.prop("disabled", false);
+        }, 1000)
+    }
+    
+    nextTurn() {
+        this.gameBoard.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+        this.startTurn();
+    }
+    
+}
+
+const boardSize = 10;
+const gameController = new GameController(boardSize);
+gameController.start();
