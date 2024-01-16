8f37fbfdb835304ac15fb756d767ebf3e0a33157
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-16 11:59:18 +0200

    create server and schema questions and api file

--- server/model/QuestionsSchema.js
+++ server/model/QuestionsSchema.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-    name: String,
-    
-})
+  type: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  correct_answer: {
+    type: String,
+    required: true,
+  },
+  incorrect_answers: {
+    type: [String],
+    required: true,
+  },
+});
 
-
-const Questions = mongoose.model("question", questionSchema)
-module.exports = Questions
+const Questions = mongoose.model("question", questionSchema);
+module.exports = Questions;
---
+++ server/model/filterData.js
@@ -0,0 +1,3 @@
+class DataHandling {
+  FilterData() {}
+}
