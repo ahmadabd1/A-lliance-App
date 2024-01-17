2046964eefe950cbc29536f246f9372e0fe860f8
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-17 15:19:44 +0200

    Update catgory route

--- server/consts.js
+++ server/consts.js
@@ -1,5 +1,9 @@
-const API_KEY = "eb1e38d9aa85222a10c2c524d923819f"
+const API_KEY = "eb1e38d9aa85222a10c2c524d923819f";
+const categoryData = {
+  9: "General Knowledge",
+  10: " Entertainment: Books",
+  12: "Entertainment: Music",
+};
 
-
-
-module.exports.API_KEY = API_KEY
+module.exports.API_KEY = API_KEY;
+module.exports.categoryData = categoryData;
