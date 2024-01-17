286856c830aba64bf6bcce48fd83e4dc48804837
Naarani <n3ra96@gmail.com>
2024-01-17 13:02:21 +0200

    checkout main failed

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,24 +4,6 @@ const Questions = require("../model/QuestionsSchema");
 const axios = require("axios");
 const dataHandling = new DataHandling();
 const router = express.Router();
-<<<<<<< HEAD
-// const saveDb = require("../model/saveDataBase");
-// const DB = new saveDb();
-
-
-
-
-router.get("/data",  (req, res) => {
-    try {
-        const promise = axios.get(
-          "https://opentdb.com/api.php?amount=10&type=multiple"
-        );
-         dataHandling.FilterData(promise, res);
-    } catch (error) {
-        console.error("Error in Fetching Data: ", error);
-        res.status(500).send({ error: "Internal Server Error" });
-    }
-=======
 router.get("/data", async (req, res) => {
   try {
     const promise = axios.get(
@@ -33,29 +15,28 @@ router.get("/data", async (req, res) => {
     console.error("Error in Fetching Data: ", error);
     res.status(500).send({ error: "Internal Server Error" });
   }
->>>>>>> main
 });
 
 router.delete("/deletedata", async (req, res) => {
-    try {
-        console.log("hi");
-        Questions.deleteMany({}).then();
-        res.send("delete the data");
-    } catch (error) {
-        console.error(error);
-        res.status(404).send({ error: "Not saved! try again!" });
-    }
+  try {
+    console.log("hi");
+    Questions.deleteMany({}).then();
+    res.send("delete the data");
+  } catch (error) {
+    console.error(error);
+    res.status(404).send({ error: "Not saved! try again!" });
+  }
 });
 
 router.get("/categories", async (req, res) => {
-    try {
-        const categories = await Questions.distinct("category");
+  try {
+    const categories = await Questions.distinct("category");
 
-        res.status(200).json({ categories });
-    } catch (error) {
-        console.error("Error fetching categories:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    res.status(200).json({ categories });
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
