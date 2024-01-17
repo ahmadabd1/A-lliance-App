838cd808f01bb37dc821414da00b42bdf26b9caf
Naarani <n3ra96@gmail.com>
2024-01-17 08:12:08 +0200

    commiting old version so i can pull the new versio

--- server/model/filterData.js
+++ server/model/filterData.js
@@ -2,25 +2,33 @@ const saveDb = require("../model/saveDataBase");
 const DB = new saveDb();
 
 class DataHandling {
-  async FilterData(promise, res) {
-    try {
-      const resp = await promise;
-      const _data = resp.data.results.map((key) => {
-        return {
-          difficulty: key.difficulty,
-          category: key.category,
-          type: key.type,
-          question: key.question,
-          correct_answer: key.correct_answer,
-          incorrect_answers: key.incorrect_answers,
-        };
+    constructor(){
+      this.data 
+    }
+   FilterData(promise, res) {
+     try {
+       const resp =  promise;
+       const _data = resp.then((response) => {
+        res.send(response.data.results);
+        let result = response.data.results
+        // console.log(result)
+        result.results.map((key) => {
+          return {
+            difficulty: key.difficulty,
+            category: key.category,
+            type: key.type,
+            question: key.question,
+            correct_answer: key.correct_answer,
+            incorrect_answers: key.incorrect_answers,
+          };
+        })
       });
       DB.saveDataToDatabase(_data);
-      console.log(_data);
-      res.send(_data);
+      
+
     } catch (error) {
-      console.error("Error in Fetching Data: ", error);
-      res.status(404).send({ error: "Try again!" });
+        console.error("Error in Fetching Data: ", error);
+        res.status(404).send({ error: "Try again!" });
     }
   }
 }
--- server/model/saveDataBase.js
+++ server/model/saveDataBase.js
@@ -4,7 +4,7 @@ class SaveDb {
   async saveDataToDatabase(data) {
     try {
       const savedQuestions = await Questions.create(data);
-      console.log("Questions saved successfully:", savedQuestions);
+      
     } catch (error) {
       console.error("Error saving questions to the database:", error);
     }
